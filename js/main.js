7d6e6337cafbcb0a10a398071548eebfdff0db73
ayumi0murata <ayumi0murata99@gmail.com>
2022-10-02 12:42:10 +0200

    delete mobile menu

--- js/main.js
+++ js/main.js
@@ -1,37 +1,4 @@
 {
-  // mobile menu window
-  const html = document.querySelector('html');
-  const headerBar = document.querySelector('#header-bar');
-  const hamburger = document.querySelector('#hamburger');
-  const mobileMenu = document.querySelector('#mobile-menu');
-  const jumpToContact = document.querySelector('#jump-to-contact');
-  let scrollpos;
-
-  function hbgAction() {
-    const hbgInview = 'hbg-inview';
-    const fixed = 'fixed';
-    headerBar.classList.toggle(hbgInview);
-    hamburger.classList.toggle(hbgInview);
-    mobileMenu.classList.toggle(hbgInview);
-    if (hamburger.classList.contains(hbgInview)) {
-      scrollpos = window.pageYOffset;
-      html.classList.add(fixed);
-      document.body.style.top = scrollpos * -1 + 'px';
-    } else {
-      html.classList.remove(fixed);
-      window.scrollTo(0, scrollpos);
-    }
-  }
-
-  hamburger.addEventListener('click', () => {
-    hbgAction();
-  });
-  jumpToContact.addEventListener('click', () => {
-    hbgAction();
-  });
-
-
-
   // animation
   const animations = document.querySelectorAll('.animation');
 
