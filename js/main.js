5e18f52dcb95728c36b900eb4eb9b0d0a10fac61
ayumi0murata <ayumi0murata99@gmail.com>
2023-10-01 12:35:15 +0200

    Changed some HTML, CSS, and JavaScript

--- js/main.js
+++ js/main.js
@@ -1,18 +1,35 @@
 {
   // animation
-  const animations = document.querySelectorAll('.animation');
-
+  const animations = document.querySelectorAll(".animation");
   const cb = (entries, observer) => {
-    entries.forEach(entry => {
+    entries.forEach((entry) => {
       if (entry.isIntersecting) {
-        entry.target.classList.add('ani-inview');
+        entry.target.classList.add("ani-inview");
         observer.unobserve(entry.target);
       }
     });
-  }
-
+  };
   const io = new IntersectionObserver(cb);
-  animations.forEach(animation => {
+  animations.forEach((animation) => {
     io.observe(animation);
   });
+
+  
+  // Get my current age
+  const myCurrentAge = document.getElementById("myCurrentAge");
+  const getAge = (year, month, day) => {
+    const today = new Date();
+    const birthDate = new Date(year, month - 1, day);
+    const currentYearBirthday = new Date(
+      today.getFullYear(),
+      birthDate.getMonth(),
+      birthDate.getDate()
+    );
+    let age = today.getFullYear() - birthDate.getFullYear();
+    if (today < currentYearBirthday) {
+      age--;
+    }
+    return age;
+  };
+  myCurrentAge.textContent = getAge(1999, 7, 29);
 }
